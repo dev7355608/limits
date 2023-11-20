aa86bdd90711533581752a2fe66baff35db13364
dev7355608 <dev7355608@gmail.com>
2023-11-20 03:16:17 +0100

    Don't limit the global light source

--- scripts/patches/light.mjs
+++ scripts/patches/light.mjs
@@ -1,5 +1,4 @@
 import { MODULE_ID } from "../const.mjs";
-import { VolumeCollection } from "../volume.mjs";
 import { Constraint } from "./../constraint.mjs";
 
 Hooks.once("libWrapper.Ready", () => {
@@ -12,29 +11,4 @@ Hooks.once("libWrapper.Ready", () => {
         libWrapper.WRAPPER,
         { perf_mode: libWrapper.PERF_FAST }
     );
-
-    libWrapper.register(
-        MODULE_ID,
-        "GlobalLightSource.prototype._createPolygon",
-        function (wrapped, ...args) {
-            return new GlobalLightSourcePolygon(wrapped(...args).points);
-        },
-        libWrapper.WRAPPER,
-        { perf_mode: libWrapper.PERF_FAST }
-    );
 });
-
-class GlobalLightSourcePolygon extends PIXI.Polygon {
-    contains(x, y) {
-        if (!super.contains(x, y)) {
-            return false;
-        }
-
-        return VolumeCollection.instance.castRay(
-            "light",
-            0,
-            x, y, 1000000,
-            x, y, -1000000
-        ).targetHit;
-    }
-}
